3cc38b08c280d8d18c66093131be7a98886a978f
Fred <freddcfonseca@gmail.com>
2022-07-25 13:25:05 -0300

    Created auditable entity

---
+++ src/main/java/com/fredfonseca/bookstoremanager/entityauditable/Auditable.java
@@ -0,0 +1,27 @@
+package com.fredfonseca.bookstoremanager.entityauditable;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(nullable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column
+    protected LocalDateTime lastModifiedDate;
+
+}
