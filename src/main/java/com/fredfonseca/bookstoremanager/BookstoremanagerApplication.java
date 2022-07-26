165f4c1046681c95e4bcd1f9f6a01585a6aab100
Fred <freddcfonseca@gmail.com>
2022-07-26 15:17:19 -0300

    Excluding servlet auto config

--- src/main/java/com/fredfonseca/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/fredfonseca/bookstoremanager/BookstoremanagerApplication.java
@@ -2,8 +2,9 @@ package com.fredfonseca.bookstoremanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BookstoremanagerApplication {
 
 	public static void main(String[] args) {
