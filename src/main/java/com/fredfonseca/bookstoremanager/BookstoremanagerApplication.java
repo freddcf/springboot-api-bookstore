29a7d6ccf2da5b8de07b31dab4f7f57ad33fae5f
Fred <freddcfonseca@gmail.com>
2022-07-26 21:23:59 -0300

    update auto deploy file

--- src/main/java/com/fredfonseca/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/fredfonseca/bookstoremanager/BookstoremanagerApplication.java
@@ -2,9 +2,8 @@ package com.fredfonseca.bookstoremanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class BookstoremanagerApplication {
 
 	public static void main(String[] args) {
