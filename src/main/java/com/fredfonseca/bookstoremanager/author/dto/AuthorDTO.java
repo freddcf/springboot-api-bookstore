5d42bfe731a9c5b47e620cd2dfc2f6e7b8179318
Fred <freddcfonseca@gmail.com>
2022-07-29 23:07:54 -0300

    Developed author by id functionality

--- src/main/java/com/fredfonseca/bookstoremanager/author/dto/AuthorDTO.java
+++ src/main/java/com/fredfonseca/bookstoremanager/author/dto/AuthorDTO.java
@@ -1,6 +1,5 @@
 package com.fredfonseca.bookstoremanager.author.dto;
 
-import com.fredfonseca.bookstoremanager.books.entity.Book;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
