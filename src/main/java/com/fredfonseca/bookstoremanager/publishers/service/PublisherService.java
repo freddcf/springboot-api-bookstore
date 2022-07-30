5cef5627020938b7ae0a1f8d2bb89b97332ecc90
Fred <freddcfonseca@gmail.com>
2022-07-30 17:54:58 -0300

    Developed structure classes for publishers module

--- src/main/java/com/fredfonseca/bookstoremanager/publishers/service/PublisherService.java
+++ src/main/java/com/fredfonseca/bookstoremanager/publishers/service/PublisherService.java
@@ -1,2 +1,19 @@
-package com.fredfonseca.bookstoremanager.publishers.service;public class PublisherService {
+package com.fredfonseca.bookstoremanager.publishers.service;
+
+import com.fredfonseca.bookstoremanager.publishers.mapper.PublisherMapper;
+import com.fredfonseca.bookstoremanager.publishers.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublisherService {
+
+    private final static PublisherMapper publisherMapper = PublisherMapper.INSTANCE;
+
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    public PublisherService(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
 }
