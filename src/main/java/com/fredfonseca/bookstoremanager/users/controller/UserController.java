44c66279335dc0e9c1c265518ed06aafa1b00bac
Fred <freddcfonseca@gmail.com>
2022-08-01 17:17:06 -0300

    Users exclusion feature

--- src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserController.java
+++ src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserController.java
@@ -26,4 +26,10 @@ public class UserController implements UserControllerDocs{
     public MessageDTO create(@RequestBody @Valid UserDTO userToCreateDTO) {
         return userService.create(userToCreateDTO);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
 }
---
+++ src/main/java/com/fredfonseca/bookstoremanager/users/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fredfonseca.bookstoremanager.users.exception;
+
+import javax.persistence.EntityNotFoundException;
+
+public class UserNotFoundException extends EntityNotFoundException {
+    public UserNotFoundException(Long id) {
+        super(String.format("User with id %s not exists", id));
+    }
+}
