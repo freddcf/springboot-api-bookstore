0555b8d9a6daabf7d770ea4c578a0498e336e0ab
Fred <freddcfonseca@gmail.com>
2022-08-02 16:46:08 -0300

    Removing security

--- src/main/java/com/fredfonseca/bookstoremanager/users/service/UserService.java
+++ src/main/java/com/fredfonseca/bookstoremanager/users/service/UserService.java
@@ -8,7 +8,6 @@ import com.fredfonseca.bookstoremanager.users.exception.UserNotFoundException;
 import com.fredfonseca.bookstoremanager.users.mapper.UserMapper;
 import com.fredfonseca.bookstoremanager.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,18 +24,14 @@ public class UserService {
 
     private UserRepository userRepository;
 
-    private PasswordEncoder passwordEncoder;
-
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public MessageDTO create(UserDTO userToCreateDTO) {
         verifyIfExists(userToCreateDTO.getEmail(), userToCreateDTO.getUsername());
         Users userToCreate = userMapper.toModel(userToCreateDTO);
-        userToCreate.setPassword(passwordEncoder.encode(userToCreate.getPassword()));
 
         Users createdUser = userRepository.save(userToCreate);
         return creationMessage(createdUser);
@@ -47,7 +42,6 @@ public class UserService {
 
         userToUpdateDTO.setId(foundUser.getId());
         Users userToUpdate = userMapper.toModel(userToUpdateDTO);
-        userToUpdate.setPassword(passwordEncoder.encode(userToUpdate.getPassword()));
 
         Users updatedUser = userRepository.save(userToUpdate);
         return updatedMessage(updatedUser);
