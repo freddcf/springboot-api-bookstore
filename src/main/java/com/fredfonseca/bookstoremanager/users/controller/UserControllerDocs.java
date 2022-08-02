830fbade714b166b8d74f653b1fbce1c9c84fe17
Fred <freddcfonseca@gmail.com>
2022-08-02 15:18:23 -0300

    Developed get users

--- src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserControllerDocs.java
+++ src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserControllerDocs.java
@@ -7,6 +7,8 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import java.util.List;
+
 @Api("System users management")
 public interface UserControllerDocs {
 
@@ -30,4 +32,17 @@ public interface UserControllerDocs {
             @ApiResponse(code = 400, message = "Missing required field, or an error on validation field rules")
     })
     MessageDTO update(Long id, UserDTO userToUpdateDTO);
+
+    @ApiOperation(value = "Find user by id operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success user found"),
+            @ApiResponse(code = 404, message = "User not found error code")
+    })
+    UserDTO findById(Long id);
+
+    @ApiOperation(value = "List all registered users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return all registered users")
+    })
+    List<UserDTO> findAll();
 }
