c390ea0466cff907fd48ceebab7f8e32ecd0c572
Fred <freddcfonseca@gmail.com>
2022-08-07 22:29:13 -0300

    Preparing for release

--- src/main/java/com/fredfonseca/bookstoremanager/rentals/exception/InvalidReturnDateException.java
+++ src/main/java/com/fredfonseca/bookstoremanager/rentals/exception/InvalidReturnDateException.java
@@ -1,11 +1,10 @@
 package com.fredfonseca.bookstoremanager.rentals.exception;
 
 import javax.persistence.EntityExistsException;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class InvalidReturnDateException extends EntityExistsException {
     public InvalidReturnDateException(LocalDate rentalDate, LocalDate returnDate) {
-        super(String.format("Return date %s should not be before rental date %s!", rentalDate, returnDate));
+        super(String.format("Return date %s should not be before rental date %s!", returnDate, rentalDate));
     }
 }
