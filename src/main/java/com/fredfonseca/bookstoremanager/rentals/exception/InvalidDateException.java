1ba7648d580bc5545ddaeb3867dd689993ab3781
Fred <freddcfonseca@gmail.com>
2022-08-13 17:58:16 -0300

    fixes:invalid date exception message

--- src/main/java/com/fredfonseca/bookstoremanager/rentals/exception/InvalidDateException.java
+++ src/main/java/com/fredfonseca/bookstoremanager/rentals/exception/InvalidDateException.java
@@ -1,10 +1,9 @@
 package com.fredfonseca.bookstoremanager.rentals.exception;
 
 import javax.persistence.EntityExistsException;
-import java.time.LocalDate;
 
 public class InvalidDateException extends EntityExistsException {
     public InvalidDateException() {
-        super("Rental date should not be in a future date ");
+        super("Return forecast may not be before the rental date");
     }
 }
