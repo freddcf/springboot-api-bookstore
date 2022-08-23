8433df6f8eac960ae6afec3d722177a82d14614e
Fred Fonseca <freddcfonseca@gmail.com>
2022-08-22 23:40:16 -0300

    fix: entities relationships

--- src/main/java/com/fredfonseca/bookstoremanager/users/entity/Users.java
+++ src/main/java/com/fredfonseca/bookstoremanager/users/entity/Users.java
@@ -1,11 +1,9 @@
 package com.fredfonseca.bookstoremanager.users.entity;
 
-import com.fredfonseca.bookstoremanager.rentals.entity.Rental;
 import com.fredfonseca.bookstoremanager.users.enums.Role;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Data
 @Entity
@@ -36,8 +34,5 @@ public class Users {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private Role role;
-
-    @OneToMany(mappedBy = "users", fetch = FetchType.LAZY)
-    private List<Rental> rentals;
 }
 
