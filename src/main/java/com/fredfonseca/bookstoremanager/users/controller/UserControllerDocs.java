bca1dce3b491d362810f61b1cd26bf663cb7e260
Fred Fonseca <freddcfonseca@gmail.com>
2022-08-26 13:35:16 -0300

    fix: remove unused import

--- src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserControllerDocs.java
+++ src/main/java/com/fredfonseca/bookstoremanager/users/controller/UserControllerDocs.java
@@ -7,10 +7,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
 
 @Api("System users management")
 public interface UserControllerDocs {
